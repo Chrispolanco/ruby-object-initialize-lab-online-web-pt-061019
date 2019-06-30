756d21c9c141c630b3ba136773dcb6326adbe5c0
Christian Polanco <chrispolanco@sbcglobal.net>
2019-06-30 20:11:32 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -11,3 +11,27 @@ class Person
     @name
   end
 end 
+
+class Dog 
+  def initialize([:name, :breed])
+    @name= name
+    @breed= breed
+  end
+  
+  def name=(name)
+    @name=name
+  end
+  
+  def name  
+    @name
+  end
+  
+  def breed=(breed)
+    @breed
+  end
+  
+  def breed
+    @breed
+  end
+  
+end
